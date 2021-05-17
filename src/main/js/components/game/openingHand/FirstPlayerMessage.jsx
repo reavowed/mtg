06ff67bbe0dda718e859aa2c40684019b86bc95f
Iain Monro <iamonro@gmail.com>
2021-05-17 11:22:29 +0100

    Remove game messages obsoleted by the log

--- src/main/js/components/game/openingHand/FirstPlayerMessage.jsx
+++
@@ -1,9 +0,0 @@
-import {useContext} from "preact/hooks";
-import GameState from "../../../GameState";
-
-export default function FirstPlayerMessage() {
-    const gameState = useContext(GameState);
-    return <h1 className="text-center mb-3">
-        {(gameState.player === gameState.gameData.playersInTurnOrder[0]) ? "You go first" : "Your opponent goes first"}
-    </h1>
-}
--- src/main/js/components/game/openingHand/OpeningHand.jsx
+++ src/main/js/components/game/openingHand/OpeningHand.jsx
@@ -2,22 +2,24 @@ import {useContext} from "preact/hooks";
 import GameState from "../../../GameState";
 import ScreenCenter from "../../layout/ScreenCenter";
 import Hand from "../Hand";
-import FirstPlayerMessage from "./FirstPlayerMessage";
 import MulliganChoice from "./MulliganChoice";
-import OpponentMulliganMessage from "./OpponentMulliganMessage";
 import ReturnCardsToLibraryChoice from "./ReturnCardsToLibraryChoice";
 
+function OpeningHandWhileWaiting() {
+    return <div>
+        <Hand />
+        <h1 className="text-center mt-3">Waiting for your opponent to make a mulligan decision</h1>
+    </div>
+}
 
 export default function OpeningHand() {
     const gameState = useContext(GameState);
-    const decision = gameState.currentChoice.playerToAct !== gameState.player ? <Hand /> :
+    const decision = gameState.currentChoice.playerToAct !== gameState.player ? <OpeningHandWhileWaiting/> :
         gameState.currentChoice.type === "MulliganChoice" ? <MulliganChoice /> :
         gameState.currentChoice.type === "ReturnCardsToLibraryChoice" ? <ReturnCardsToLibraryChoice /> :
         <Hand />;
     return <ScreenCenter>
         <div className="w-100">
-            <FirstPlayerMessage />
-            <OpponentMulliganMessage />
             {decision}
         </div>
     </ScreenCenter>;
