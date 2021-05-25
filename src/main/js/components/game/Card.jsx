fe45501da79caf6f5b5abdf729318f3f82ed5f29
Iain Monro <iamonro@gmail.com>
2021-05-25 15:15:10 +0100

    Refactor action manager so priority choice element controls the menu

--- src/main/js/components/game/Card.jsx
+++ src/main/js/components/game/Card.jsx
@@ -1,40 +1,8 @@
-import {useContext, useEffect, useState} from "preact/hooks";
+import {useCallback, useContext, useEffect, useState} from "preact/hooks";
 import ActionManager from "../../contexts/ActionManager";
-import DecisionMaker from "../../contexts/DecisionMaker";
 import ScryfallService from "../../contexts/ScryfallService";
 import $ from "jQuery";
 import {useRefWithEventHandler} from "../../utils/hook-utils";
-
-function ActionText({text}) {
-    const elements = [];
-    while (text !== "") {
-        if (text.startsWith("{")) {
-            const closingBraceIndex = text.indexOf("}")
-            const symbolContents = text.substring(1, closingBraceIndex);
-            elements.push(<span className={"card-symbol card-symbol-" + symbolContents}/>);
-            text = text.substring(closingBraceIndex + 1);
-        } else {
-            const openingBraceIndex = text.indexOf("{");
-            if (openingBraceIndex > -1) {
-                elements.push(text.substring(0, openingBraceIndex))
-                text = text.substring(openingBraceIndex);
-            } else {
-                elements.push(text);
-                text = "";
-            }
-        }
-    }
-    return elements;
-}
-
-function ActionMenu({actions, ...props}) {
-    const decisionMaker = useContext(DecisionMaker);
-    return <div class="dropdown-menu dropdown-menu-sm" id="context-menu" {...props} >
-        {actions.map(action => <a class="dropdown-item" href="#" key={action.option} onclick={() => decisionMaker.makeDecision(action.option)}>
-            <ActionText text={action.text} />
-        </a>)}
-    </div>
-}
 
 export default function Card({card, ...props}) {
     const scryfallService = useContext(ScryfallService);
@@ -42,29 +10,18 @@ export default function Card({card, ...props}) {
     useEffect(() => scryfallService.requestCard(card, setScryfallCard), []);
 
     const actionManager = useContext(ActionManager);
-    const actions = actionManager.getActions(card.objectId);
-    const [actionMenuDetails, setActionMenuDetails] = useState(null);
-    function handleClick(event) {
-        const details = {
-            left: event.pageX,
-            top: event.pageY
-        }
-        setActionMenuDetails(details);
-        event.originalEvent.fromHandler = handleClick;
-        $(document).on("click", handleDocumentClick);
-    }
-    function handleDocumentClick(event) {
-        if (event.originalEvent.fromHandler !== handleClick) setActionMenuDetails(null);
-    }
+    const handleClick = useCallback((event) => {
+        actionManager.actionHandler(card.objectId, event);
+    }, [card, actionManager]);
+
     function addImageHandler(img) {
-        actions && actions.length > 0 && $(img).on("click", handleClick);
+        $(img).on("click", handleClick);
     }
     function removeImageHandler(img) {
         $(img).off("click", handleClick);
-        $(document).off("click", handleDocumentClick);
     }
 
-    const imgRef = useRefWithEventHandler(addImageHandler, removeImageHandler, [actions]);
+    const imgRef = useRefWithEventHandler(addImageHandler, removeImageHandler, [handleClick]);
 
     const containerClasses = ["cardContainer"]
     if (card.permanentStatus && card.permanentStatus.isTapped) {
@@ -73,6 +30,5 @@ export default function Card({card, ...props}) {
 
     return scryfallCard && <div className={containerClasses.join(" ")}>
         <img ref={imgRef} src={scryfallCard.image_uris.small} {...props} />
-        {actionMenuDetails && <ActionMenu actions={actions} style={{display: "block", position: "fixed", top: actionMenuDetails.top, left: actionMenuDetails.left }} />}
     </div>
 }
--- src/main/js/contexts/ActionManager.js
+++ src/main/js/contexts/ActionManager.js
@@ -1,36 +1,15 @@
 import {createContext} from "preact";
-import {useCallback, useContext, useEffect, useState} from "preact/hooks";
-import GameState from "./GameState";
-import _ from "lodash";
+import {useCallback, useState} from "preact/hooks";
 
 const ActionManager = createContext(null);
 const InternalProvider = ActionManager.Provider;
 
-function getActionsFromGameState(gameState) {
-    const actions = {};
-    function addAction(objectId, action) {
-        actions[objectId] = actions[objectId] || [];
-        actions[objectId] = [...actions[objectId], action];
-    }
-    _.forEach(gameState.currentChoice.details.availableActions, action => addAction(action.objectId, {
-        text: action.displayText,
-        option: action.optionText
-    }));
-    return actions;
-}
-
 ActionManager.Provider = function({children}) {
-    const gameState = useContext(GameState);
-    const [actionsByObjectId, setActionsByObjectId] = useState(null);
-    useEffect(() => {
-        setActionsByObjectId(getActionsFromGameState(gameState))
-    }, [gameState]);
-    const getActions = useCallback((objectId) => {
-        if (!gameState.currentChoice || gameState.currentChoice.type !== "PriorityChoice" || gameState.currentChoice.playerToAct !== gameState.player)
-            return [];
-        return actionsByObjectId[objectId] || [];
-    }, [actionsByObjectId]);
-    return actionsByObjectId && <InternalProvider value={{getActions}}>{children}</InternalProvider>;
+    const [rawActionHandler, setActionHandler] = useState(null);
+    const actionHandler = useCallback((objectId, event) => {
+        rawActionHandler && rawActionHandler(objectId, event);
+    }, [rawActionHandler])
+    return <InternalProvider value={{actionHandler, setActionHandler}}>{children}</InternalProvider>;
 }
 
 export default ActionManager;
