afe51752b7c297a875d69a82e63e0bdf183732d2
Iain Monro <iamonro@gmail.com>
2022-03-15 08:54:43 +0000

    Remove type restriction on Identifiers

--- src/main/scala/mtg/effects/identifiers/MultipleIdentifier.scala
+++ src/main/scala/mtg/effects/identifiers/MultipleIdentifier.scala
@@ -1,11 +1,10 @@
 package mtg.effects.identifiers
 
-import mtg.core.ObjectOrPlayerId
 import mtg.effects.StackObjectResolutionContext
 import mtg.game.state.GameState
 import mtg.text.NounPhrase
 
-trait MultipleIdentifier[+T <: ObjectOrPlayerId] {
+trait MultipleIdentifier[+T] {
   def getAll(gameState: GameState, resolutionContext: StackObjectResolutionContext): (Seq[T], StackObjectResolutionContext)
   def getNounPhrase(cardName: String): NounPhrase
   def getText(cardName: String): String = getNounPhrase(cardName).text
--- src/main/scala/mtg/effects/identifiers/SingleIdentifier.scala
+++ src/main/scala/mtg/effects/identifiers/SingleIdentifier.scala
@@ -4,7 +4,7 @@ import mtg.core.ObjectOrPlayerId
 import mtg.effects.StackObjectResolutionContext
 import mtg.game.state.GameState
 
-trait SingleIdentifier[+T <: ObjectOrPlayerId] extends MultipleIdentifier[T] {
+trait SingleIdentifier[+T] extends MultipleIdentifier[T] {
   def get(gameState: GameState, resolutionContext: StackObjectResolutionContext): (T, StackObjectResolutionContext)
   def getAll(gameState: GameState, resolutionContext: StackObjectResolutionContext): (Seq[T], StackObjectResolutionContext) = {
     get(gameState, resolutionContext).mapLeft(Seq(_))
