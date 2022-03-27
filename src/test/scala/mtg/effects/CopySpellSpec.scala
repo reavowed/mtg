257c0945a6047f0ec6b117776dc6bf0373ab5a91
Iain Monro <iamonro@gmail.com>
2022-03-27 18:41:17 +0100

    Extract CopyTriggerSpec

--- src/test/scala/mtg/effects/CopySpellSpec.scala
+++ src/test/scala/mtg/effects/CopySpellSpec.scala
@@ -1,38 +1,18 @@
 package mtg.effects
 
-import mtg.abilities.TriggeredAbilityDefinition
 import mtg.abilities.builder.InstructionBuilder._
-import mtg.cards.patterns.{ArtifactCard, SpellCard}
+import mtg.cards.patterns.SpellCard
 import mtg.core.types.Type
 import mtg.game.objects.{AbilityOnTheStack, CopyOfSpell}
 import mtg.game.turns.TurnPhase
 import mtg.helpers.SpecWithTestCards
-import mtg.instructions.actions.{Copy, DrawACard, GainLife}
-import mtg.instructions.articles.A
-import mtg.instructions.conditions.When
-import mtg.instructions.nouns.{Spell, You}
+import mtg.instructions.actions.{Copy, GainLife}
+import mtg.instructions.nouns.You
 import mtg.parts.costs.ManaCost
 
 class CopySpellSpec extends SpecWithTestCards {
-  object TestCopyCard extends SpellCard(
-    "Copy Card",
-    ManaCost(0),
-    Type.Instant,
-    Nil,
-    Copy(target(spell)))
-  object TestGainOneLifeCard extends SpellCard(
-    "Lifegain Card",
-    ManaCost(0),
-    Type.Instant,
-    Nil,
-    You(GainLife(1)))
-
-  object TestCopyTriggerArtifact extends ArtifactCard(
-    "Copy Trigger Artifact",
-    ManaCost(0),
-    When(You, Copy, A(Spell))(DrawACard))
-
-  override def testCards = Seq(TestCopyCard, TestGainOneLifeCard, TestCopyTriggerArtifact)
+  import CopySpellSpec._
+  override def testCards = Seq(TestCopyCard, TestGainOneLifeCard)
 
   "copying a spell" should {
     "duplicate a simple spell" in {
@@ -48,24 +28,6 @@ class CopySpellSpec extends SpecWithTestCards {
       manager.gameState.gameObjectState.stack.size mustEqual 2
       manager.gameState.gameObjectState.stack(1).underlyingObject must beAnInstanceOf[CopyOfSpell]
       manager.gameState.gameObjectState.stack(1).underlyingObject.baseCharacteristics.name must beSome(TestGainOneLifeCard.name)
-    }
-
-    "trigger an appropriate ability" in {
-      val initialState = emptyGameObjectState
-        .setHand(playerOne, Seq(TestCopyCard, TestGainOneLifeCard))
-        .setBattlefield(playerOne, TestCopyTriggerArtifact)
-
-      implicit val manager = createGameStateManagerAtStartOfFirstTurn(initialState)
-      manager.passUntilPhase(TurnPhase.PrecombatMainPhase)
-      manager.castSpell(playerOne, TestGainOneLifeCard)
-      manager.castSpell(playerOne, TestCopyCard)
-      manager.chooseCard(playerOne, TestGainOneLifeCard)
-      manager.resolveNext()
-
-      manager.gameState.gameObjectState.stack.size mustEqual 3
-      manager.gameState.gameObjectState.stack(2).underlyingObject must beAnInstanceOf[AbilityOnTheStack]
-      manager.gameState.gameObjectState.stack(2).underlyingObject.asInstanceOf[AbilityOnTheStack].abilityDefinition mustEqual
-        TestCopyTriggerArtifact.textParagraphs.head
     }
   }
 
@@ -85,5 +47,21 @@ class CopySpellSpec extends SpecWithTestCards {
       manager.gameState.gameObjectState.lifeTotals(playerOne) mustEqual initialLifeTotal + 2
     }
   }
+
+}
+
+object CopySpellSpec {
+  object TestCopyCard extends SpellCard(
+    "Copy Card",
+    ManaCost(0),
+    Type.Instant,
+    Nil,
+    Copy(target(spell)))
+  object TestGainOneLifeCard extends SpellCard(
+    "Lifegain Card",
+    ManaCost(0),
+    Type.Instant,
+    Nil,
+    You(GainLife(1)))
 
 }
---
+++ src/test/scala/mtg/triggers/CopyTriggerSpec.scala
@@ -0,0 +1,43 @@
+package mtg.triggers
+
+import mtg.cards.patterns.ArtifactCard
+import mtg.effects.CopySpellSpec.{TestCopyCard, TestGainOneLifeCard}
+import mtg.game.objects.AbilityOnTheStack
+import mtg.game.turns.TurnPhase
+import mtg.helpers.SpecWithTestCards
+import mtg.instructions.actions.{Copy, DrawACard}
+import mtg.instructions.articles.A
+import mtg.instructions.conditions.When
+import mtg.instructions.nouns.{Spell, You}
+import mtg.parts.costs.ManaCost
+
+class CopyTriggerSpec extends SpecWithTestCards {
+
+  object TestCopyTriggerArtifact extends ArtifactCard(
+    "Copy Trigger Artifact",
+    ManaCost(0),
+    When(You, Copy, A(Spell))(DrawACard))
+
+  override def testCards = Seq(TestCopyCard, TestGainOneLifeCard, TestCopyTriggerArtifact)
+
+  "a copy trigger" should {
+    "trigger off copying a spell"  in {
+      val initialState = emptyGameObjectState
+        .setHand(playerOne, Seq(TestCopyCard, TestGainOneLifeCard))
+        .setBattlefield(playerOne, TestCopyTriggerArtifact)
+
+      implicit val manager = createGameStateManagerAtStartOfFirstTurn(initialState)
+      manager.passUntilPhase(TurnPhase.PrecombatMainPhase)
+      manager.castSpell(playerOne, TestGainOneLifeCard)
+      manager.castSpell(playerOne, TestCopyCard)
+      manager.chooseCard(playerOne, TestGainOneLifeCard)
+      manager.resolveNext()
+
+      manager.gameState.gameObjectState.stack.size mustEqual 3
+      manager.gameState.gameObjectState.stack(2).underlyingObject must beAnInstanceOf[AbilityOnTheStack]
+      manager.gameState.gameObjectState.stack(2).underlyingObject.asInstanceOf[AbilityOnTheStack].abilityDefinition mustEqual
+        TestCopyTriggerArtifact.textParagraphs.head
+    }
+  }
+
+}
