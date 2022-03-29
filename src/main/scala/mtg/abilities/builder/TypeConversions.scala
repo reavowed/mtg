b9f9d0e2fd30dcc2226e5ab80b0b2d1ef7086750
Iain Monro <iamonro@gmail.com>
2022-03-29 15:02:59 +0100

    Dedicated TypePhrase that is both a noun and an adjective

--- src/main/scala/mtg/abilities/builder/TypeConversions.scala
+++ src/main/scala/mtg/abilities/builder/TypeConversions.scala
@@ -3,16 +3,12 @@ package mtg.abilities.builder
 import mtg.core.ObjectId
 import mtg.core.types.Type
 import mtg.effects.condition.Condition
-import mtg.instructions.{CharacteristicChangingVerb, Instruction, SuffixDescriptor}
-import mtg.instructions.adjectives.Adjective
+import mtg.instructions.{CharacteristicChangingVerb, Instruction, SuffixDescriptor, TypePhrase}
 import mtg.instructions.nounPhrases.{PluralNoun, SetIdentifyingNounPhrase}
 import mtg.instructions.nouns.Noun
 
-object TypeConversions extends LowPriorityTypeConversions {
-  implicit class TypeExtensions(t: Type) {
-    def apply(noun: Noun[ObjectId]): Noun[ObjectId] = typeToAdjective(t)(noun)
-    def apply(suffixDescriptor: SuffixDescriptor): Noun[ObjectId] = typeToNoun(t)(suffixDescriptor)
-  }
+object TypeConversions {
+  implicit def typeToPhrase(t: Type): TypePhrase = TypePhrase(t)
   implicit class ObjectPhraseExtensions(phrase: SetIdentifyingNounPhrase[ObjectId]) {
     def apply(verb: CharacteristicChangingVerb, endCondition: Condition): Instruction = {
       CharacteristicChangingVerb.WithSubjectAndCondition(phrase, verb, endCondition)
@@ -24,9 +20,4 @@ object TypeConversions extends LowPriorityTypeConversions {
     }
     def apply(suffixDescriptor: SuffixDescriptor): Noun[ObjectId] = Noun.WithSuffix(noun, suffixDescriptor)
   }
-}
-
-trait LowPriorityTypeConversions {
-  implicit def typeToAdjective(t: Type): Adjective = Adjective.TypeAdjective(t)
-  implicit def typeToNoun(t: Type): Noun[ObjectId] = Noun.TypeNoun(t)
 }
---
+++ src/main/scala/mtg/instructions/TypePhrase.scala
@@ -0,0 +1,27 @@
+package mtg.instructions
+
+import mtg.core.ObjectId
+import mtg.core.types.Type
+import mtg.effects.EffectContext
+import mtg.effects.condition.Condition
+import mtg.game.state.{Characteristics, GameState}
+import mtg.instructions.adjectives.Adjective
+import mtg.instructions.nounPhrases.PluralNoun
+import mtg.instructions.nouns.{Noun, Permanent}
+import mtg.utils.TextUtils._
+
+case class TypePhrase(types: Type*) extends Noun[ObjectId] with Adjective with Descriptor.CharacteristicDescriptor {
+  override def getText(cardName: String): String = types.map(_.name.toLowerCase).toCommaList("or")
+  override def getSingular(cardName: String): String = getText(cardName)
+  override def getAll(gameState: GameState, effectContext: EffectContext): Seq[ObjectId] = {
+    apply(Permanent).getAll(gameState, effectContext)
+  }
+  override def describes(characteristics: Characteristics, gameState: GameState, effectContext: EffectContext): Boolean = {
+    types.forall(characteristics.types.contains)
+  }
+
+  def apply(verb: CharacteristicChangingVerb, endCondition: Condition): Instruction = {
+    CharacteristicChangingVerb.WithSubjectAndCondition(PluralNoun(this), verb, endCondition)
+  }
+  def apply(suffixDescriptor: SuffixDescriptor): Noun[ObjectId] = Noun.WithSuffix(this, suffixDescriptor)
+}
