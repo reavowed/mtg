81a2d7248ccf0cf487aea8c828f09415a452d258
Iain Monro <iamonro@gmail.com>
2022-04-01 15:34:00 +0100

    Make lessons selectable

--- src/main/js/components/choices/LearnChoice.jsx
+++ src/main/js/components/choices/LearnChoice.jsx
@@ -1,4 +1,4 @@
-import {useContext} from "preact/hooks";
+import {useContext, useState} from "preact/hooks";
 import GameState from "../../contexts/GameState";
 import CardRow from "../card/CardRow";
 import CardWithText from "../card/CardWithText";
@@ -9,12 +9,22 @@ import find from "lodash/find"
 
 export default function LearnChoice() {
     const gameState = useContext(GameState);
-    const validLessonIds = distinct(gameState.currentChoice.details.possibleLessons);
+    const [chosenLesson, setChosenLesson] = useState(null);
     const getId = ({artDetails: {set, collectorNumber}}) => set + "-" + collectorNumber;
+
+    function ChoosableLesson({card, ...props}) {
+        const id = getId(card);
+        const isChosen = id === chosenLesson;
+        const className = isChosen ? "selected" : "selectable";
+        const onClick = () => setChosenLesson(id);
+        return <CardWithText card={card} onClick={onClick} className={className} {...props} />
+    }
+
+    const validLessonIds = distinct(gameState.currentChoice.details.possibleLessons);
     const validLessons = validLessonIds.map(id => find(gameState.sideboard, card => getId(card) === id));
 
     return <PopupChoice text="Learn">
         <BannerText>Learn</BannerText>
-        <CardRow cards={validLessons} as={CardWithText} />
+        <CardRow cards={validLessons} as={ChoosableLesson} />
     </PopupChoice>;
 }
