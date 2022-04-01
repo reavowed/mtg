a28c3165aa56be91744d6e0e88ba8d51989ce8d7
Iain Monro <iamonro@gmail.com>
2022-04-01 15:29:09 +0100

    Filter learn choice to valid lessons

--- src/main/js/components/choices/LearnChoice.jsx
+++ src/main/js/components/choices/LearnChoice.jsx
@@ -4,11 +4,17 @@ import CardRow from "../card/CardRow";
 import CardWithText from "../card/CardWithText";
 import BannerText from "../layoutUtils/BannerText";
 import PopupChoice from "./PopupChoice";
+import distinct from "lodash/uniq"
+import find from "lodash/find"
 
 export default function LearnChoice() {
     const gameState = useContext(GameState);
+    const validLessonIds = distinct(gameState.currentChoice.details.possibleLessons);
+    const getId = ({artDetails: {set, collectorNumber}}) => set + "-" + collectorNumber;
+    const validLessons = validLessonIds.map(id => find(gameState.sideboard, card => getId(card) === id));
+
     return <PopupChoice text="Learn">
         <BannerText>Learn</BannerText>
-        <CardRow cards={gameState.sideboard} as={CardWithText} />
+        <CardRow cards={validLessons} as={CardWithText} />
     </PopupChoice>;
 }
